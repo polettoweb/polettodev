bcb35582ce7c11885eefb48f87dc9ebb8394ce37
Marco Poletto <marco@momice.com>
2020-01-21 17:48:18 +0100

    Added uses page

---
+++ src/pages/uses.vue
@@ -0,0 +1,91 @@
+<template>
+  <Layout>
+    <div class="container-inner mx-auto py-16">
+      <h2 class="text-4xl font-bold mb-8">Current setup</h2>
+      <p class="text-xl mb-8">Current tech and hardware that I use every day for my job.</p>
+      <div>
+        <h2 class="text-2xl font-bold">Hardware</h2>
+        <ul>
+          <li class="text-copy-secondary ml-4 mb-4">
+            <strong>Mac Mini 2018</strong> (3.2 GHz i7, 16 GB 2667 MHz, 512GB, AMD 5700 XT via Thunderbolt 3)
+          </li>
+          <li class="text-copy-secondary ml-4 mb-4">
+            <strong>Apple Magic Keyboard 2</strong> (Space gray)
+          </li>
+          <li class="text-copy-secondary ml-4 mb-4">
+            <strong>Logitech MX Master</strong>
+          </li>
+          <li class="text-copy-secondary ml-4 mb-4">
+            <strong>AORUS AD27QD</strong> 1440p monitor
+          </li>
+          <li class="text-copy-secondary ml-4 mb-4">
+            <strong>iPad pro 12.9"</strong> (256GB space grey)
+          </li>
+          <li class="text-copy-secondary ml-4 mb-4">
+            <strong>Pixel 2 XL</strong>
+          </li>
+        </ul>
+        <h2 class="text-2xl font-bold">Development Tools</h2>
+        <ul>
+          <li class="text-copy-secondary ml-4 mb-4">
+            <strong>VS Code</strong> with
+            <strong>Monokai PRO</strong> theme and
+            <strong>Dank Mono</strong> as font
+          </li>
+          <li class="text-copy-secondary ml-4 mb-4">
+            <strong>iTerm2</strong> Terminal with ohMyZsh and Powerlevel10k skin
+          </li>
+        </ul>
+        <h2 class="text-2xl font-bold">Productivity</h2>
+        <ul>
+          <li class="text-copy-secondary ml-4 mb-4">
+            <strong>Notability</strong> for notes taking
+          </li>
+          <li class="text-copy-secondary ml-4 mb-4">
+            <strong>Jira</strong> for project management
+          </li>
+          <li class="text-copy-secondary ml-4 mb-4">
+            <strong>Slack</strong> for communication
+          </li>
+          <li class="text-copy-secondary ml-4 mb-4">
+            <strong>Procreate</strong> for sketching
+          </li>
+        </ul>
+        <h2 class="text-2xl font-bold">Other stuff</h2>
+        <ul>
+          <li class="text-copy-secondary ml-4 mb-4">
+            <strong>Google Chrome</strong> (Canary) for development
+          </li>
+          <li class="text-copy-secondary ml-4 mb-4">
+            <strong>Bitwarden</strong> as my password manager
+          </li>
+          <li class="text-copy-secondary ml-4 mb-4">
+            <strong>Magnet</strong> to snap windows into places
+          </li>
+        </ul>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title:
+      "Page Not Found | 404 | Poletto.dev | Marco Poletto | Web Developer | Mentor",
+    meta: [
+      { charset: "utf-8" },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
+      },
+      { name: "author", content: "Marco Poletto Web Developer" },
+      {
+        name: "description",
+        content:
+          "Marco Poletto Frontend Web Developer, Tech enthusiast, Coffee aficionado, Gamer, mentor, tutor, 404, page not found"
+      }
+    ]
+  }
+};
+</script>
