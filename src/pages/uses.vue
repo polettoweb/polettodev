49140c4c9ab27f328bced8e6a673d0523c30afee
Marco Poletto <polettoweb@gmail.com>
2022-07-08 11:20:45 +0200

    Updated uses page

--- src/pages/uses.vue
+++ src/pages/uses.vue
@@ -2,35 +2,36 @@
   <Layout>
     <div class="container mx-auto py-16">
       <h2 class="text-4xl font-bold mb-8">Current setup</h2>
-      <p class="text-xl mb-8">Current tech and hardware that I use every day for my job.</p>
+      <p class="text-xl mb-8">
+        Current tech and hardware that I use every day for my job.
+      </p>
       <div>
         <h2 class="text-2xl font-bold">Hardware</h2>
         <ul>
           <li class="text-copy-secondary ml-4 mb-4">
-            <strong>Mac Mini 2018</strong> (3.2 GHz i7, 16 GB 2667 MHz, 512GB, AMD 5700 XT via Thunderbolt 3)
+            <strong>MacBook Pro 16 2021</strong> (M1 Pro)
           </li>
           <li class="text-copy-secondary ml-4 mb-4">
             <strong>Apple Magic Keyboard 2</strong> (Space gray)
           </li>
           <li class="text-copy-secondary ml-4 mb-4">
-            <strong>Logitech MX Master</strong>
+            <strong>SteelSeries Aerox 9 Wireless</strong>
           </li>
           <li class="text-copy-secondary ml-4 mb-4">
-            <strong>AORUS AD27QD</strong> 1440p monitor
+            <strong>LG 38GN950</strong> 3840x1600 ultrawide monitor
           </li>
           <li class="text-copy-secondary ml-4 mb-4">
-            <strong>iPad pro 12.9"</strong> (256GB space grey)
+            <strong>iPad pro 12.9"</strong> (M1 256GB space grey)
           </li>
           <li class="text-copy-secondary ml-4 mb-4">
-            <strong>Pixel 2 XL</strong>
+            <strong>iPhone 11 128GB</strong>
           </li>
         </ul>
         <h2 class="text-2xl font-bold">Development Tools</h2>
         <ul>
           <li class="text-copy-secondary ml-4 mb-4">
-            <strong>VS Code</strong> with
-            <strong>Monokai PRO</strong> theme and
-            <strong>Dank Mono</strong> as font
+            <strong>VS Code</strong> with<strong> Night Owl</strong> theme
+            and<strong> Dank Mono</strong> as font
           </li>
           <li class="text-copy-secondary ml-4 mb-4">
             <strong>iTerm2</strong> Terminal with ohMyZsh and Powerlevel10k skin
@@ -39,10 +40,10 @@
         <h2 class="text-2xl font-bold">Productivity</h2>
         <ul>
           <li class="text-copy-secondary ml-4 mb-4">
-            <strong>Notability</strong> for notes taking
+            <strong>Notion</strong> for notes taking
           </li>
           <li class="text-copy-secondary ml-4 mb-4">
-            <strong>Jira</strong> for project management
+            <strong>Linear</strong> for project management
           </li>
           <li class="text-copy-secondary ml-4 mb-4">
             <strong>Slack</strong> for communication
@@ -54,7 +55,7 @@
         <h2 class="text-2xl font-bold">Other stuff</h2>
         <ul>
           <li class="text-copy-secondary ml-4 mb-4">
-            <strong>Google Chrome</strong> (Canary) for development
+            <strong>Google Chrome</strong> for development
           </li>
           <li class="text-copy-secondary ml-4 mb-4">
             <strong>Bitwarden</strong> as my password manager
@@ -77,15 +78,15 @@ export default {
       { charset: "utf-8" },
       {
         name: "viewport",
-        content: "width=device-width, initial-scale=1"
+        content: "width=device-width, initial-scale=1",
       },
       { name: "author", content: "Marco Poletto Web Developer" },
       {
         name: "description",
         content:
-          "Marco Poletto Frontend Web Developer, Tech enthusiast, Coffee aficionado, Gamer, mentor, tutor, 404, page not found"
-      }
-    ]
-  }
+          "Marco Poletto Frontend Web Developer, Tech enthusiast, Coffee aficionado, Gamer, mentor, tutor, 404, page not found",
+      },
+    ],
+  },
 };
 </script>
